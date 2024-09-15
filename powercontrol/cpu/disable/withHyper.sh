33845983dc86b10e106dfefdc42dc572fa4c4a6f
ssk015 <18252677381@163.com>
2024-09-15 06:51:10 +0000

    update socket close with SMT

--- powercontrol/cpu/disable/withHyper.sh
+++ powercontrol/cpu/disable/withHyper.sh
@@ -10,11 +10,9 @@ three_quarters=$((3 * total_cpus / 4))
 echo "Total CPUs: $total_cpus"
 echo "Offline CPUs: $quarter-$(($half - 1)) and $three_quarters-$(($total_cpus - 1))"
 
-# 禁用 25%-50% 范围的 CPU 核心
+
 for ((i = quarter; i < half; i++)); do
-    if [ -f /sys/devices/system/cpu/cpu$i/online ]; then
-        # echo "0" | sudo tee /sys/devices/system/cpu/cpu$i/online
-        echo "1" | sudo tee /sys/devices/system/cpu/cpu$i/online
+        # echo "1" | sudo tee /sys/devices/system/cpu/cpu$i/online
         # echo $i
         echo "CPU $i set to offline."
     else
@@ -25,8 +23,8 @@ done
 # 禁用 75%-100% 范围的 CPU 核心
 for ((i = three_quarters; i < total_cpus; i++)); do
     if [ -f /sys/devices/system/cpu/cpu$i/online ]; then
-        # echo "0" | sudo tee /sys/devices/system/cpu/cpu$i/online
-        echo "1" | sudo tee /sys/devices/system/cpu/cpu$i/online
+        echo "0" | sudo tee /sys/devices/system/cpu/cpu$i/online
+        # echo "1" | sudo tee /sys/devices/system/cpu/cpu$i/online
         # echo $i
         echo "CPU $i set to offline."
     else
